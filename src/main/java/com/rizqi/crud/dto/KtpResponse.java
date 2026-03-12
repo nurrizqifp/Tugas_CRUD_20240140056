fe14e79f87b8e09d26d41f04c2056ad287b8d6dd
nurrizqifp <nurrizqifebriansyah@gmail.com>
2026-03-12 21:07:56 +0700

    Add code: variables

--- src/main/java/com/rizqi/crud/dto/KtpResponse.java
+++ src/main/java/com/rizqi/crud/dto/KtpResponse.java
@@ -1,4 +1,12 @@
 package com.rizqi.crud.dto;
 
+import java.time.LocalDate;
+
 public class KtpResponse {
+    private Integer id;
+    private String nomorKtp;
+    private String namaLengkap;
+    private String alamat;
+    private LocalDate tanggalLahir;
+    private String jenisKelamin;
 }
