91aa3f96dbaf86c510350d5f890d6145980ecc35
nurrizqifp <nurrizqifebriansyah@gmail.com>
2026-03-12 21:05:24 +0700

    Add code: add variables

--- src/main/java/com/rizqi/crud/dto/KtpRequest.java
+++ src/main/java/com/rizqi/crud/dto/KtpRequest.java
@@ -1,4 +1,24 @@
 package com.rizqi.crud.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.time.LocalDate;
+
 public class KtpRequest {
+    @NotBlank(message = "nomorKtp wajib diisi")
+    @Size(max = 50)
+    private String nomorKtp;
+
+    @NotBlank(message = "namaLengkap wajib diisi")
+    @Size(max = 255)
+    private String namaLengkap;
+
+    private String alamat;
+
+    @NotNull(message = "tanggalLahir wajib diisi")
+    private LocalDate tanggalLahir;
+
+    @NotBlank(message = "jenisKelamin wajib diisi")
+    private String jenisKelamin;
 }
