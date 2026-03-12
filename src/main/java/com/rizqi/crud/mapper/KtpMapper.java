427d226914e6e789df6f8773360b5f3413106ecb
nurrizqifp <nurrizqifebriansyah@gmail.com>
2026-03-13 04:25:24 +0700

    Add code to KtpMapper: toResponse

--- src/main/java/com/rizqi/crud/mapper/KtpMapper.java
+++ src/main/java/com/rizqi/crud/mapper/KtpMapper.java
@@ -1,4 +1,19 @@
 package com.rizqi.crud.mapper;
 
+import com.rizqi.crud.entity.Ktp;
+import com.rizqi.crud.dto.KtpRequest;
+import com.rizqi.crud.dto.KtpResponse;
+
 public class KtpMapper {
+    public static KtpResponse toResponse(Ktp ktp) {
+        if (ktp == null) return null;
+        KtpResponse r = new KtpResponse();
+        r.setId(ktp.getId());
+        r.setNomorKtp(ktp.getNomorKtp());
+        r.setNamaLengkap(ktp.getNamaLengkap());
+        r.setAlamat(ktp.getAlamat());
+        r.setTanggalLahir(ktp.getTanggalLahir());
+        r.setJenisKelamin(ktp.getJenisKelamin());
+        return r;
+    }
 }
