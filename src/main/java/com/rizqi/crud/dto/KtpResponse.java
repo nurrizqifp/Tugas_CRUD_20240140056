6b2f85fb097f819a319dbc0c9e258264cb5e32d3
nurrizqifp <nurrizqifebriansyah@gmail.com>
2026-03-12 21:08:32 +0700

    Add code: getters & setters

--- src/main/java/com/rizqi/crud/dto/KtpResponse.java
+++ src/main/java/com/rizqi/crud/dto/KtpResponse.java
@@ -9,4 +9,17 @@ public class KtpResponse {
     private String alamat;
     private LocalDate tanggalLahir;
     private String jenisKelamin;
+    // getters & setters
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+    public String getNomorKtp() { return nomorKtp; }
+    public void setNomorKtp(String nomorKtp) { this.nomorKtp = nomorKtp; }
+    public String getNamaLengkap() { return namaLengkap; }
+    public void setNamaLengkap(String namaLengkap) { this.namaLengkap = namaLengkap; }
+    public String getAlamat() { return alamat; }
+    public void setAlamat(String alamat) { this.alamat = alamat; }
+    public LocalDate getTanggalLahir() { return tanggalLahir; }
+    public void setTanggalLahir(LocalDate tanggalLahir) { this.tanggalLahir = tanggalLahir; }
+    public String getJenisKelamin() { return jenisKelamin; }
+    public void setJenisKelamin(String jenisKelamin) { this.jenisKelamin = jenisKelamin; }
 }
