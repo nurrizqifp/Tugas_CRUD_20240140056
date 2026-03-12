64f292b1803d538182c5d2e22941f3e597318e9d
nurrizqifp <nurrizqifebriansyah@gmail.com>
2026-03-12 20:49:58 +0700

    Konfigurasi awal dependensi, environment variabel dan database

--- src/main/java/com/rizqi/crud/util/DotenvConfig.java
+++ src/main/java/com/rizqi/crud/util/DotenvConfig.java
@@ -1,4 +1,17 @@
 package com.rizqi.crud.util;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class DotenvConfig {
+
+    public static void load() {
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMalformed()
+                .ignoreIfMissing()
+                .load();
+
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
+    }
 }
