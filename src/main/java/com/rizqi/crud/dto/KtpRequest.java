dfcadb21124d2d313ebf3f4b1767ebd47d5131e0
nurrizqifp <nurrizqifebriansyah@gmail.com>
2026-03-12 21:05:48 +0700

    Add code: getters & setters

--- src/main/java/com/rizqi/crud/dto/KtpRequest.java
+++ src/main/java/com/rizqi/crud/dto/KtpRequest.java
@@ -21,4 +21,16 @@ public class KtpRequest {
 
     @NotBlank(message = "jenisKelamin wajib diisi")
     private String jenisKelamin;
+
+    // getters & setters
+    public String getNomorKtp() { return nomorKtp; }
+    public void setNomorKtp(String nomorKtp) { this.nomorKtp = nomorKtp; }
+    public String getNamaLengkap() { return namaLengkap; }
+    public void setNamaLengkap(String namaLengkap) { this.namaLengkap = namaLengkap; }
+    public String getAlamat() { return alamat; }
+    public void setAlamat(String alamat) { this.alamat = alamat; }
+    public LocalDate getTanggalLahir() { return tanggalLahir; }
+    public void setTanggalLahir(LocalDate tanggalLahir) { this.tanggalLahir = tanggalLahir; }
+    public String getJenisKelamin() { return jenisKelamin; }
+    public void setJenisKelamin(String jenisKelamin) { this.jenisKelamin = jenisKelamin; }
 }
