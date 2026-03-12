ec653e2b53d950c5fb3d5139228854eae54d0733
nurrizqifp <nurrizqifebriansyah@gmail.com>
2026-03-13 04:30:26 +0700

    Add code to interface class: KtpService

--- src/main/java/com/rizqi/crud/service/KtpService.java
+++ src/main/java/com/rizqi/crud/service/KtpService.java
@@ -1,4 +1,13 @@
 package com.rizqi.crud.service;
 
+import com.rizqi.crud.dto.KtpRequest;
+import com.rizqi.crud.dto.KtpResponse;
+import java.util.List;
+
 public interface KtpService {
+    KtpResponse create(KtpRequest request);
+    List<KtpResponse> findAll();
+    KtpResponse findById(Integer id);
+    KtpResponse update(Integer id, KtpRequest request);
+    void delete(Integer id);
 }
